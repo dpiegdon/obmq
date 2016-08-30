c892e037827e49590909b2ac2e60d4c37d33f842
David R. Piegdon <dgit@piegdon.de>
2016-08-30 00:49:09 +0000

    example cleanup

--- example.c
+++ example.c
@@ -6,9 +6,12 @@
 
 static unsigned time = 0;
 
+//#define PLOTTABLE_OUTPUT
+//#define DEBUG_QUEUE
+
 void setchan(void * data, char value)
 {
-#if 0
+#ifdef PLOTTABLE_OUTPUT
 	// for gnuplot testing:
 	static char v;
 	printf("%d %d\n", time*2, v);
@@ -27,19 +30,30 @@ int main()
 	char *p = message;
 
 	obmq_init(&m, setchan, NULL, 0, 0, 0, 8, 0);
+#ifdef PLOTTABLE_OUTPUT
+	fprintf(stderr, "you can interrupt after a second. then plot via:\n"
+			"  gnuplot> plot 'outfile' using 1:2 with linespoints\n");
+#else
 	sleep(1);
+#endif
 
 	while(1) {
 		if(*p && obmq_messages_free(&m)) {
+#ifdef debug_queue
 			fprintf(stderr, "free %u queued %u.\n", obmq_messages_free(&m), obmq_messages_queued(&m));
+#endif
 			obmq_queuemessage(&m, *p);
+#ifdef debug_queue
 			fprintf(stderr, "queued message %u.\n", (unsigned)*p);
+#endif
 			++p;
 		}
 
 		obmq_trigger(&m);
 
+#ifndef PLOTTABLE_OUTPUT
 		usleep(50000);
+#endif
 		++time;
 	}
 }
